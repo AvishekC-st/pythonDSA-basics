b0cea69679deb114c3c0d96ad7ce79a8287050e5
Avishek Chandra <avishekchandra90@yahoo.com>
2023-01-17 23:33:26 -0800

    Initial Commits.

---
+++ Exercise1_variables_and_printing_output.py
@@ -0,0 +1,7 @@
+"""For this exercise, please assign an object (e.g., a number)
+ to the x variable in the first line.
+ Then, print out the variable value in the second line."""
+
+x = 9
+print(x)
+

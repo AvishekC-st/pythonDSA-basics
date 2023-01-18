e3ff29dda879caea28f074f8ac60c2fbd8dfab6e
Avishek Chandra <avishekchandra90@yahoo.com>
2023-01-17 23:57:10 -0800

    Added more excercies

---
+++ Exercise6 _create_complex_list.py
@@ -0,0 +1,7 @@
+"""Assigning a list to the rainfall variable.
+ The list should contain four items, a float,
+  an integer, a string, and a list*.
+
+*It's totally fine for a list to contain another list as an item."""
+
+rainfall = [4.5, 4, '4', []]

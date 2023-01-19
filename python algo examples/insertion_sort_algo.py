d2a8abf34d8314d94edd58b49884056063943055
Avishek Chandra <avishekchandra90@yahoo.com>
2023-01-19 13:53:36 -0800

    Added more excercies

---
+++ python algo examples/insertion_sort_algo.py
@@ -0,0 +1,26 @@
+"""Insertion sort
+-Best case run time is O(n)
+-Worst case run time is O(n^2)
+"""
+
+
+def insertion_sort(array):
+    """we start at 2nd index of the arr and iterate over length of the arry"""
+    for i in range(1, len(array)):
+        """we set j to i-1 because it will always be behind i and enver cross it"""
+        j = i-1
+        """The first part of the while loop ensures that J will never go left out of bounds
+         and the second portion compares the two elements and makes sure that
+         if the J + 1 element is smaller than J we decrement and swap
+         if J+1 is not smaller the loop ends and assumes all elements are sorted"""
+        while j >= 0 and array[j+1] < array[j]:
+            """swapping notation so that if J+1 is smaller than j we swap the two
+            elements then decrement j pointer with j -= 1 """
+            array[j+1], array[j] = array[j], array[j+1]
+            j -= 1
+    return array
+
+
+arr = [2, 5, 7, 3, 4, 1]
+
+print(insertion_sort(arr))

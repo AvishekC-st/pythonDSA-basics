314109ed927a68a1816646ec9c6e200f1327527b
Avishek Chandra <avishekchandra90@yahoo.com>
2023-02-13 22:10:20 -0800

    Added 125.Valid Palidrome to leetcode will add description tomorrow

---
+++ Leetcode/125, .Valid Palindrome.py
@@ -0,0 +1,5 @@
+class Solution:
+    def isPalindrome(self, s):
+        newS= [i.lower() for i in s if i.isalnum()]
+        return newS == newS[::-1]
+    #return newS[:len(newS)/2] == newS[(len(newS)+1)/2:][::-1]  # This one is better, but too long

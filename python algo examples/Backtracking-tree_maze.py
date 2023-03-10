63fe2f593381eef0e61f853020f3febbcb97e85b
Avishek Chandra <avishekchandra90@yahoo.com>
2023-03-09 22:48:43 -0800

    worked on depth first search file

---
+++ python algo examples/Backtracking-tree_maze.py
@@ -0,0 +1,10 @@
+"""
+Time Complexity
+
+Given that the tree has  n nodes, the time complexity will be O(n) because we have to traverse the whole tree.
+Just like any brute force algorithm, we will have to traverse the while input, which in this case is just the size
+of the tree.
+
+
+
+"""
--- test.py
+++ test.py
@@ -1,5 +1,17 @@
-def print_left_oriented_triangle(height, symbol):
-    for i in range(1, height + 1):
-        print(symbol * i)
-
-print_left_oriented_triangle(6,"*")
+n=5
+for i in range(n-1):
+    for j in range(i, n):
+        print(' ', end=' ')
+    for j in range(i):
+        print('*', end=' ')
+    for j in range(i+1):
+        print('*', end=' ')
+    print()
+for i in range(n):
+    for j in range(i+1):
+        print(' ', end=' ')
+    for j in range(i,n-1):
+        print('*', end=' ')
+    for j in range(i,n):
+        print('*', end=' ')
+    print()
